fdda3eac40c7838b3e3c861e59c8eb03c21436d4
Steven <stevennguyen571@gmail.com>
2023-12-11 23:23:02 -0600

    10

--- problems/10-my-every.js
+++ problems/10-my-every.js
@@ -22,7 +22,13 @@ console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
 *******************************************************************************/
 
 function myEvery(array, cb) {
-    // Your code here 
+    let bool = true;
+    array.forEach(ele => {
+        if(!cb(ele)){
+            bool = false;
+        }
+    })
+    return bool;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
